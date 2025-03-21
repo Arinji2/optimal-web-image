6535fb027a536d604cf482a166974cc3825df7e1
Arinji2 <arinjaydhar205@gmail.com>
2025-03-21 23:16:33 +0530

    Add Getting Data, Creating Folder

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -18,4 +20,27 @@ func main() {
 		fmt.Println("Error: File does not exist.")
 		os.Exit(1)
 	}
+
+	extension := filepath.Ext(filePath)
+	fileName, found := strings.CutSuffix(filepath.Base(filePath), extension)
+	if !found {
+		fmt.Println("Error: File has no extension.")
+		os.Exit(1)
+	}
+	extension = strings.ReplaceAll(extension, ".", "")
+	fileLoc := filepath.Dir(filePath)
+	fmt.Println("Extension:", extension)
+	fmt.Println("File Name:", fileName)
+	fmt.Println("File Location:", fileLoc)
+	fileLoc = filepath.Join(fileLoc, fileName)
+
+	err := os.Mkdir(fileLoc, 0755)
+	if err != nil {
+		fmt.Println("Error: Failed to create directory:", fileLoc)
+		os.Exit(1)
+	}
+	defer func() {
+		fmt.Println("Removing Created Folder")
+		os.Remove(fileLoc)
+	}()
 }
